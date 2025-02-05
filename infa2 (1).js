75dd6fd62dbcee56de3e2702958c04de4d03cd54
quenixxx <topstorm@mail.ru>
2025-02-05 22:40:45 +0300

    fwe

---
+++ infa2 (1).js
@@ -0,0 +1,40 @@
+// function checkNumber(X) {
+//     let str = String(X)
+//     let sum1 = Number(str[0]) + Number(str[1])
+//     let sum2 = Number(str[2]) + Number(str[3])
+//     console.log(sum1 === sum2)
+// }
+// checkNumber(1221)
+
+
+// function equilateralTriangle(a, b, c) {
+//     console.log(a === b && b === c)
+// }
+// equilateralTriangle(4,4,4)
+
+
+
+// function composition(a, b, c) {
+//     console.log((a * b) % c === 0)
+// }
+// composition(4,6,8)
+
+
+
+// function sumNumbers(X) {
+//     let str = String(X)
+//     let sum = Number(str[0]) + Number(str[1]) + Number(str[2])
+//     console.log(sum % 2 === 0)
+// } 
+// sumNumbers(246)
+
+
+
+// function notebook(D, K, M, P) {
+//     let cost = D * K
+//     if (K > M) {
+//         cost -= cost * (P / 100)
+//     }
+//     console.log(cost)
+// }
+// notebook(10, 5, 3 , 10)
